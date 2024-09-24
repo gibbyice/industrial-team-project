db655c4fdbb025bb05bc53bfdf61e3a1f49863a7
naomisilver <2425281@dundee.ac.uk>
2024-09-24 13:31:04 +0100

    Added settings.php and a bunch to style.css :airplane:
    smiling

---
+++ src/settings.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Settings</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="../styles/style.css">
+  </head>
+  <body>
+    <div class="title-container">
+      <div class="title">
+          <h1 class="oswaldMedium scaling-text">Settings</h1>
+      </div>
+      <div class="divider"></div>
+    </div>
+
+    <div class="body-container">
+      <div class="buttons">
+        <div class="d-grid gap-2">
+          <button class="btn btn-outline-light dynamic-button" type="button">FAQ</button>
+          <button class="btn btn-outline-light dynamic-button" type="button">Log out</button>
+        </div>
+      </div>
+    </div>
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
