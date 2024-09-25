01dcd1635bab2fea0395c494b34825c1507dfdb7
Lewis McLean <45165834+LewisMcLean2@users.noreply.github.com>
2024-09-25 11:03:38 +0100

    Added 'Get user information' and 'Add user' API calls

--- api/index.js
+++ api/index.js
@@ -34,9 +34,40 @@ app.get('/hello', (req, res) => {
 // Returns a user's greenscore based on the account number provided
 app.get('/:userID/greenscore', (req, res) => {
   var userID = req.params.userID
-  connection.one(`SELECT Green_Score FROM Account WHERE userid = ${userID}`)
+  connection.one(`SELECT Green_Score FROM users WHERE userid = ${userID};`)
   .then((data) => {
-    console.log('DATA:', data.value)
+    console.log('DATA: ', data)
+  })
+  .catch((error) => {
+    console.log('ERROR:', error)
+  })
+})
+
+//Returns a user's information based on the user ID provided
+app.get('/Account/:userID', (req, res) => {
+  var userID = req.params.userID
+  connection.one(`SELECT * FROM users WHERE userid = ${userID};`)
+  .then((data) => {
+    res.json(data)
+  })
+  .catch((error) => {
+    console.log('ERROR:', error)
+  })
+})
+
+//Adds a user to the database using the parameters as the user's information.
+app.get('/AddAccount/:userID/:name/:balance/:greenscore/:carbon/:waste/:sustainability/:category', (req, res) => {
+  var userID = req.params.userID
+  var name = req.params.name
+  var balance = req.params.balance
+  var greenscore = req.params.greenscore
+  var carbon = req.params.carbon
+  var waste = req.params.waste
+  var sustainability = req.params.sustainability
+  var category = req.params.category
+  connection.one(`INSERT INTO users (userID, name, balance, Green_Score, streak, carbon_emissions, waste_management, sustainability_practices, category) VALUES (${userID}, ${name}, ${balance}, ${greenscore}, 0, ${carbon}, ${waste}, ${sustainability}, ${category}) RETURNING *;`, )
+  .then((data) => {
+    res.json(data)
   })
   .catch((error) => {
     console.log('ERROR:', error)
