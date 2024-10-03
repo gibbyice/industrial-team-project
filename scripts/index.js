73b4c9601ca323c8d2f033279fad9c116c75bb6e
rossfergs <2449478@dundee.ac.uk>
2024-10-03 16:29:25 +0100

    removed  company list from home

--- scripts/index.js
+++ scripts/index.js
@@ -1,97 +1,9 @@
 const APIaddress = "http://34.201.132.70:3000/";
-var currentPage = 0
-var maxPages = -1 // used to prevent pagination going beyond the total number of companies
 
 window.onload = init() // runs fetchPayee immediatly on page load
 
 function init(){
     getAccDetails()
-    getCompanies()
-}
-
-function getMaxPages(){
-    fetch(APIaddress+`maxPageCount`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            maxPages = data.maxPages
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-}
-
-function nextPage(){
-    getMaxPages() // refreshes every time in case new companies added
-    if (currentPage != maxPages-1){
-        currentPage ++
-        getCompanies()
-    } else {
-        console.log("next denied")
-    }
-}
-
-function prevPage(){
-    if (currentPage != 0){
-        currentPage --
-        getCompanies()
-    } else {
-        console.log("prev denied")
-    }
-}
-
-function getCompanies(){
-    fetch(APIaddress+`getAllCompanies/${currentPage}`)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
-    .then(result => {
-        // Do the thing here
-        document.getElementById('current-page').innerHTML = `Page ${currentPage+1}`
-        document.getElementById('companies-list').innerHTML = "" // removes any companies alr in list
-        for (let i = 0; i < result.data.length; i++) { 
-            generateCompanyListItem(result.data[i])
-        }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
-}
-
-function generateCompanyListItem(data){
-
-    comapnyName = document.createElement("h3");
-    comapnyName.setAttribute('class', 'mb-5 text-center')
-    comapnyName.innerHTML = data.name
-
-    companyCategory = document.createElement("h3");
-    companyCategory.setAttribute('class', 'mb-5 text-center')
-    companyCategory.innerHTML = data.category
-
-    listItem = document.createElement("a");
-    listItem.setAttribute('href', `companyDetails.php?companyID=${data.userid}`);
-    // Apply colour based on EIS
-    comapnyEIS = data.carbon_emissions + data.waste_management + data.sustainability_practices
-    if (comapnyEIS > 20){
-        listItem.setAttribute('class', 'display-unit d-flex justify-content-between nav-link green-bg')
-    } else if (comapnyEIS > 10){
-        listItem.setAttribute('class', 'display-unit d-flex justify-content-between nav-link yellow-bg')
-    } else {
-        listItem.setAttribute('class', 'display-unit d-flex justify-content-between nav-link')
-    }
-
-    listItem.appendChild(comapnyName)
-    listItem.appendChild(companyCategory)
-
-    companyList = document.getElementById('companies-list') // Get list
-    companyList.appendChild(listItem)
 }
 
 function getAccDetails(){
