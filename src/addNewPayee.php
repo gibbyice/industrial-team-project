35776639dd04d30aa0df4a739e4da4d89def1af7
rossfergs <2449478@dundee.ac.uk>
2024-10-03 16:40:01 +0100

    added spacer to adding payee screen

--- src/addNewPayee.php
+++ src/addNewPayee.php
@@ -63,6 +63,7 @@
             </div>
         </div>
     </div>
+    <div class="manual-spacer" style="height: 170px;"></div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="../scripts/payment.js"></script>
     <script src="../scripts/addNewPayee.js" type="text/javascript"></script>
