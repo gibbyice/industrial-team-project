7e7020dff8891a181af13f5e82cb8d586d337e17
Lewis McLean <45165834+LewisMcLean2@users.noreply.github.com>
2024-10-03 18:35:38 +0100

    Details popups now display all transaction details

--- src/recentTransactions.php
+++ src/recentTransactions.php
@@ -56,10 +56,5 @@
 </body>
 <script src="../scripts/RecentTransactions.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-<script>
-
-    // Resetting the carousel to its first frame
-    
-</script>
 
 </html>
